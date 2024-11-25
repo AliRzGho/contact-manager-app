a5520ec49d52160aa2d687da7016df680e2653bb
AliRzGho <googlialireza@gmail.com>
2024-11-25 10:42:30 +0330

    First Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h3>اپلیکیشن مدیر یت مخاطبین</h3>
+      <span className="fa fa-user"></span>
+
+      <button className="btn btn-primary">button</button>
+    </div>
+    //test
+  );
+};
+
+export default App;
