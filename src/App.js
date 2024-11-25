7d763d68036a499d55cbbbee45d9232e32522ba4
AliRzGho <googlialireza@gmail.com>
2024-11-25 11:46:31 +0330

    change

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
+import Navbar from "./components/Navbar";
+
 import "./App.css";
 
 const App = () => {
   return (
     <div className="App">
-      <h3>اپلیکیشن مدیر یت مخاطبین</h3>
-      <span className="fa fa-user"></span>
-
-      <button className="btn btn-primary">button</button>
+      <Navbar />
     </div>
-    //test
   );
 };
 
